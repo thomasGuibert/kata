19ca4ee38adaac147e9f60090d8c19dbafb41c54
thomasGuibert <guibert.th@gmail.com>
2026-01-21 17:47:49 +0100

    first commit for setup

--- Agicap/KataAgicapTests/UnitTest1.cs
+++ Agicap/KataAgicapTests/UnitTest1.cs
@@ -1,9 +1,13 @@
-﻿namespace KataAgicapTests;
+﻿using Shouldly;
+
+namespace KataAgicapTests;
 
 public class UnitTest1
 {
+
     [Fact]
     public void Test1()
     {
+        true.ShouldBe(false);
     }
 }
